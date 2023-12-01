2085749c6c26ef94876bb6f41e21e930fa5a5dcc
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-12-01 10:05:50 -0500

    started making seprated files to put in what my queries were for the answers

---
+++ part2.sql
@@ -0,0 +1,12 @@
+--1
+SELECT *
+FROM types
+
+--2
+SELECT *
+FROM pokemons
+WHERE id = 45;
+
+--3
+SELECT count(*) as number_of_pokemon
+FROM pokemons
---
+++ part3.sql
@@ -0,0 +1,35 @@
+--1.
+SELECT *
+FROM pokemons
+LEFT JOIN types
+ON pokemons.primary_type=types.ID;
+
+--2.
+SELECT *
+FROM pokemons
+LEFT JOIN types
+ON pokemons.secondary_type=types.ID
+WHERE pokemons.name = 'Rufflet';
+
+--3
+SELECT name
+FROM  pokemons
+LEFT JOIN pokemon_trainer
+ON pokemon_trainer.pokemon_id=pokemons.id
+WHERE pokemon_trainer.trainerID = 303;
+
+--4
+SELECT *
+FROM pokemons
+LEFT JOIN types
+ON pokemons.secondary_type=types.ID
+WHERE types.name = 'Poison';
+
+--5
+SELECT *
+FROM pokemons
+LEFT JOIN types
+ON pokemons.primary_type=types.ID
+WHERE types.id = 1;
+
+
