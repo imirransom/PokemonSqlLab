0643470bfede2ba6c37d05549c05042059011aea
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-12-01 10:09:23 -0500

    done adding all the part 2 queries

--- part2.sql
+++ part2.sql
@@ -10,3 +10,11 @@ WHERE id = 45;
 --3
 SELECT count(*) as number_of_pokemon
 FROM pokemons
+
+--4
+SELECT count(*) as number_of_types
+FROM types;
+
+--5
+SELECT count(secondary_type) as count_of_secondaries
+FROM pokemons;
